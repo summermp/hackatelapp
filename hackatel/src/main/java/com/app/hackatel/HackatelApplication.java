a9e34ade957982330e6e7a7f0f5b4d629535d04d
Sam <summermp>
2021-12-17 00:42:32 -0500

    Creación de la aplicación web hackatel

---
+++ hackatel/src/main/java/com/app/hackatel/HackatelApplication.java
@@ -0,0 +1,13 @@
+package com.app.hackatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatelApplication.class, args);
+	}
+
+}
---
+++ hackatel/src/test/java/com/app/hackatel/HackatelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hackatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
